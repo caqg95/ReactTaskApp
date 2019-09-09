fdf7413d728972601cd217edbc37f77910f69d47
caqg95 <cquinterogrande@yahoo.es>
2019-09-09 00:57:41 -0600

    Updates

---
+++ precache-manifest.eab0c5d613948c5144b8bf5e2a05560b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2b54e6213aa7e39fd32e2932221999",
+    "url": "/ReactTaskApp/index.html"
+  },
+  {
+    "revision": "5a18072bc75897b7e731",
+    "url": "/ReactTaskApp/static/css/main.023ef077.chunk.css"
+  },
+  {
+    "revision": "e05f1891367f1f50ee66",
+    "url": "/ReactTaskApp/static/js/2.1430f7f0.chunk.js"
+  },
+  {
+    "revision": "5a18072bc75897b7e731",
+    "url": "/ReactTaskApp/static/js/main.c8d321b4.chunk.js"
+  },
+  {
+    "revision": "2f08a12f45b905445f64",
+    "url": "/ReactTaskApp/static/js/runtime~main.7c20b601.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTaskApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9697cf4418ba7997bf50dcb4ff074de4.js"
+  "/ReactTaskApp/precache-manifest.eab0c5d613948c5144b8bf5e2a05560b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactTaskApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
