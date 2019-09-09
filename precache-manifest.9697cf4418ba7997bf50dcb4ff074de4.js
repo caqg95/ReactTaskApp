9417981a03eabcd3de854a64bfee0b785ae6aef7
caqg95 <cquinterogrande@yahoo.es>
2019-09-09 00:37:03 -0600

    Updates

---
+++ precache-manifest.9697cf4418ba7997bf50dcb4ff074de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbbc5947166d0e1edaac9d2aa0f168e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6f803894e3dfcfb606",
+    "url": "/static/css/main.023ef077.chunk.css"
+  },
+  {
+    "revision": "e05f1891367f1f50ee66",
+    "url": "/static/js/2.1430f7f0.chunk.js"
+  },
+  {
+    "revision": "1a6f803894e3dfcfb606",
+    "url": "/static/js/main.8f8d428c.chunk.js"
+  },
+  {
+    "revision": "33fe6b284369c96f2db6",
+    "url": "/static/js/runtime~main.9ae4ea14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
